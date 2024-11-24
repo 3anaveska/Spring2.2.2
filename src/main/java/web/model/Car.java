ee4f2e2f82398599a46d59edc5e783b03dd8ae09
3anaveska <dmitri_goba@mail.ru>
2024-11-24 14:31:18 +0300

    Initial commit

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,40 @@
+package web.Util;
+
+public class Car {
+
+    private String marka;
+    private String color;
+    private int year;
+    public Car() {
+    }
+
+    public Car(String marka, String color, int year) {
+        this.marka = marka;
+        this.color = color;
+        this.year = year;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
